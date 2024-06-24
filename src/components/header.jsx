b6913998146916642c5e8a2a871410a943a170d9
kollakek <kollatopbro@gmail.com>
2024-06-24 17:54:38 +0800

    .

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,12 +21,12 @@ export default function Header() {
     }, [lastScrollY]);
 
 
-    const headerClass = isVisible ? 'top-6' : '-top-24';
+    const headerClass = isVisible ? 'max-sm:top-0 top-6' : '-top-24';
     return (
-        <header className={`transition-all duration-700 ${headerClass} bg-white/15 backdrop-blur container mx-auto fixed inset-x-0 py-3 px-4 card h-min z-30`}>
+        <header className={`transition-all duration-700 ${headerClass} border border-white/10 bg-white/10 backdrop-blur container mx-auto fixed inset-x-0 py-3 px-4 sm:card h-min z-30`}>
             <div className="flex justify-between items-center">
                 <div className="flex gap-3">
-                    <h1 className="text-3xl font-bold my-auto">VND<span className="text-primary">team</span></h1>
+                    <a href="/" className="text-3xl font-bold my-auto">VND<span className="text-primary">team</span></a>
                     <div className="h-8 w-[1px] bg-white/20 my-auto"></div>
                     <div className="max-lg:hidden">
                         <a href="/" className="my-auto btn btn-ghost text-xl">Главная</a>
@@ -35,7 +35,7 @@ export default function Header() {
                     </div>
                 </div>
                 <div className="flex gap-2">
-                    <a href="/login" className="btn btn-primary my-auto text-base">Заказать</a>
+                    <a href="/" className="btn btn-primary my-auto text-base">Заказать</a>
                 </div>
             </div>
         </header>
