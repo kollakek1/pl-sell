09e61665d24445828c13b1d7655c98ef19d3f0db
kollakek <kollatopbro@gmail.com>
2024-06-24 17:15:33 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()],
+  integrations: [react(), tailwind(), icon(), sitemap(), robotsTxt()],
+  site: 'https://vnd-team.vercel.app',
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
