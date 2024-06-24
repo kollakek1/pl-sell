f78c57e5c2cc16455c6e15450bbfe74107a1b814
kollakek <kollatopbro@gmail.com>
2024-06-24 16:58:45 +0800

    ю

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), icon()]
+  integrations: [react(), tailwind(), icon()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
