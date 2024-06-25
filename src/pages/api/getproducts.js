b0e2df522c2a28f39e7090c4d451a74eb1f1456e
kollakek <kollatopbro@gmail.com>
2024-06-25 22:57:23 +0800

    .

--- src/pages/api/getproducts.js
+++ src/pages/api/getproducts.js
@@ -1,10 +1,7 @@
 import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export async function GET() {
-  const uri = process.env.MONGODB_URI;
+  const uri = import.meta.env.MONGODB_URI;
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
