c7488b03e125879a926365217d49e4cba752a2d0
kollakek <kollatopbro@gmail.com>
2024-06-25 11:42:59 +0800

    Update engpage.js

--- public/engpage.js
+++ public/engpage.js
@@ -1,10 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', function () {
+
+  const redirected = localStorage.getItem('en');
+  
+  if (!redirected) {
     const userLang = navigator.language || navigator.userLanguage; 
     const userLangPrefix = userLang.split('-')[0];
-  
+
+
     if (userLangPrefix !== 'ru') {
       window.location.href = '/en';
     }
-  });
-  
+
+    localStorage.setItem('en', 'true');
+  }
+});
