8362e77413521f1f75079afd2e0727074ad87c07
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-06-29 19:59:23 +0800

    .

--- public/engpage.js
+++ public/engpage.js
@@ -1,13 +1,11 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Проверяем, было ли перенаправление уже выполнено
   const redirected = localStorage.getItem('redirected');
 
   if (!redirected) {
     const userLang = navigator.language || navigator.userLanguage; 
     const userLangPrefix = userLang.split('-')[0];
 
-    // Если язык пользователя не русский, перенаправляем на другую страницу
     if (userLangPrefix !== 'ru') {
       window.location.href = '/en';
       localStorage.setItem('redirected', 'true');
