704fb5da995709db04267124d85992a7d4c3e350
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-06-30 12:55:44 +0800

    Update main.jsx

--- src/components/services/main.jsx
+++ src/components/services/main.jsx
@@ -83,7 +83,7 @@ export default function Main() {
                                 <svg className="mt-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 24 24">
                                     <path fillRule="evenodd" d="M5 5a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2H5Zm9 2a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H14Zm3 0a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17ZM3 17v-3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm11-2a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H14Zm3 0a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clipRule="evenodd"/>
                                 </svg>
-                                <p className="ml-2">Сервер</p>
+                                <p className="ml-2">Minecraft сервер</p>
                             </h1>
                             <p className="text-lg mt-2 text-base-content/75">Разработка сервера под ваши нужды</p>
                         </div>
