14e6322d475d6289cbea0482b83385931a2727bd
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-07-01 11:43:12 +0800

    Update index.js

--- src/pages/api/login/index.js
+++ src/pages/api/login/index.js
@@ -15,28 +15,40 @@ export async function POST({ request }) {
         const productsCollection = mongoDb.collection('userproduct');
         const products = await productsCollection.find({ email: userEmail }).toArray();
     
+        console.log('User products:', products); // Логирование
+    
         const productDetailsCollection = mongoDb.collection('products');
     
+        // Итоговый массив для продуктов с добавленной информацией
         const enrichedProducts = [];
     
         for (const userProduct of products) {
-
+            // Получаем _id продукта из userproduct
             const { orderId, key } = userProduct;
-            
-
-            const productDetails = await productDetailsCollection.findOne({ _id: new ObjectId(orderId) });
-            
+    
+            console.log('Processing user product:', userProduct); // Логирование
+    
+            // Находим соответствующий продукт в коллекции products
+            const productDetails = await productDetailsCollection.findOne({ _id: orderId });
+    
+            console.log('Found product details:', productDetails); // Логирование
+    
             if (productDetails) {
-
+                // Добавляем информацию о продукте в итоговый массив
                 enrichedProducts.push({
                     ...productDetails,
-                    ...(key && { key }),
+                    ...(key && { key }), // Добавляем key, если он существует
                 });
             }
         }
     
+        console.log('Enriched products:', enrichedProducts); // Логирование
+    
         return new Response(JSON.stringify(enrichedProducts));
+    } else {
+        console.log('Invalid code'); // Логирование
     }
+    
     
 
     return new Response(false);
