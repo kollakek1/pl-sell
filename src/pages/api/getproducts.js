71b620f981a8cada85e56a2e45b333a530637b06
kollakek <78259478+kollakek1@users.noreply.github.com>
2024-07-14 18:08:28 +0800

    1

--- src/pages/api/getproducts.js
+++ src/pages/api/getproducts.js
@@ -2,6 +2,9 @@ import { connectToMongo } from '../../lib/mongodb';
 
 export async function GET() {
   const { db } = await connectToMongo();
+  if (!db) {
+    throw new Error('Failed to connect to the database');
+  }
   const productsCollection = db.collection('products');
   const products = await productsCollection.find().toArray();
   for (const product of products) {
